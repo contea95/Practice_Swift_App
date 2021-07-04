43bbdb7bec2cb075f6b019c432130d220ed2e62b
Sang Hyeok Han <contea95@naver.com>
2021-07-04 23:44:40 +0900

    14_MoviePlayer

---
+++ 14_MoviePlayer/14_MoviePlayer/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  14_MoviePlayer
+//
+//  Created by 한상혁 on 2021/07/04.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        // 내부 파일 mp4
+        let filePath: String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+        // 외부 파일 mp4
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+    }
+    
+    private func playVideo(url: NSURL) {
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true){
+            player.play()
+        }
+    }
+}
+

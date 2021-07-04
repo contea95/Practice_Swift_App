2438d8b9fca9de08eb2986d1de82b85576c2d317
Sang Hyeok Han <contea95@naver.com>
2021-07-05 00:16:51 +0900

    14_MoviePlayer_ADD other Vidio

--- 14_MoviePlayer/14_MoviePlayer/ViewController.swift
+++ 14_MoviePlayer/14_MoviePlayer/ViewController.swift
@@ -22,10 +22,21 @@ class ViewController: UIViewController {
         
         playVideo(url: url)
     }
+    @IBAction func btnPlayInernalMovFile(_ sender: UIButton) {
+        let filePath: String? = Bundle.main.path(forResource: "Mountaineering", ofType: "mov")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
     
     @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
         // 외부 파일 mp4
         let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+    }
+    @IBAction func btnPlayExternalMovFile(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/ijybpprsmx0bgre/Seascape.mov")!
         
         playVideo(url: url)
     }

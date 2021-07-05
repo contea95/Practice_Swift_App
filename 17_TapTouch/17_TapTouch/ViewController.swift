37f1dbd5ba26cf6d68d7261ab295ca8d3bab1620
Sang Hyeok Han <contea95@naver.com>
2021-07-06 01:01:50 +0900

    17_TapTouch+Sketch

---
+++ 17_TapTouch/17_TapTouch/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  17_TapTouch
+//
+//  Created by 한상혁 on 2021/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var txtMessage: UILabel!
+    @IBOutlet var txtTapCount: UILabel!
+    @IBOutlet var txtTouchCount: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Began"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Moved"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Ended"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+
+
+}
+

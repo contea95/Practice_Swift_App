1ef7736f4d3274c32039c16dbfe514a42bc6842f
Sang Hyeok Han <contea95@naver.com>
2021-07-07 03:07:14 +0900

    18_SwipeGesture

---
+++ 18_SwipeGesture/18_SwipeGesture/ViewController.swift
@@ -0,0 +1,131 @@
+//
+//  ViewController.swift
+//  18_SwipeGesture
+//
+//  Created by 한상혁 on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let numOfTouchs = 2
+
+    @IBOutlet var imgViewUp: UIImageView!
+    @IBOutlet var imgViewDown: UIImageView!
+    @IBOutlet var imgViewLeft: UIImageView!
+    @IBOutlet var imgViewRight: UIImageView!
+    
+    var imgLeft = [UIImage]()
+    var imgRight = [UIImage]()
+    var imgUp = [UIImage]()
+    var imgDown = [UIImage]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imgUp.append(UIImage(named: "arrow-up-black.png")!)
+        imgUp.append(UIImage(named: "arrow-up-red.png")!)
+        imgUp.append(UIImage(named: "arrow-up-green.png")!)
+        imgDown.append(UIImage(named: "arrow-down-black.png")!)
+        imgDown.append(UIImage(named: "arrow-down-red.png")!)
+        imgDown.append(UIImage(named: "arrow-down-green.png")!)
+        imgLeft.append(UIImage(named: "arrow-left-black.png")!)
+        imgLeft.append(UIImage(named: "arrow-left-red.png")!)
+        imgLeft.append(UIImage(named: "arrow-left-green.png")!)
+        imgRight.append(UIImage(named: "arrow-right-black.png")!)
+        imgRight.append(UIImage(named: "arrow-right-red.png")!)
+        imgRight.append(UIImage(named: "arrow-right-green.png")!)
+        
+        imgViewUp.image = imgUp[0]
+        imgViewDown.image = imgDown[0]
+        imgViewLeft.image = imgLeft[0]
+        imgViewRight.image = imgRight[0]
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+//        swipeUp.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+//        swipeDown.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeDown)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+//        swipeLeft.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+//        swipeRight.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUpMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeMultiGesture(_:)))
+        swipeUpMulti.direction = UISwipeGestureRecognizer.Direction.up
+        swipeUpMulti.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeUpMulti)
+        
+        let swipeDownMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeMultiGesture(_:)))
+        swipeDownMulti.direction = UISwipeGestureRecognizer.Direction.down
+        swipeDownMulti.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeDownMulti)
+        
+        let swipeLeftMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeMultiGesture(_:)))
+        swipeLeftMulti.direction = UISwipeGestureRecognizer.Direction.left
+        swipeLeftMulti.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeLeftMulti)
+        
+        let swipeRightMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeMultiGesture(_:)))
+        swipeRightMulti.direction = UISwipeGestureRecognizer.Direction.right
+        swipeRightMulti.numberOfTouchesRequired = numOfTouchs
+        self.view.addGestureRecognizer(swipeRightMulti)
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            imgViewUp.image = imgUp[0]
+            imgViewDown.image = imgDown[0]
+            imgViewLeft.image = imgLeft[0]
+            imgViewRight.image = imgRight[0]
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.up:
+                imgViewUp.image = imgUp[1]
+            case UISwipeGestureRecognizer.Direction.down:
+                imgViewDown.image = imgDown[1]
+            case UISwipeGestureRecognizer.Direction.left:
+                imgViewLeft.image = imgLeft[1]
+            case UISwipeGestureRecognizer.Direction.right:
+                imgViewRight.image = imgRight[1]
+            default:
+                break
+            }
+        }
+    }
+    
+    @objc func respondToSwipeMultiGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            imgViewUp.image = imgUp[0]
+            imgViewDown.image = imgDown[0]
+            imgViewLeft.image = imgLeft[0]
+            imgViewRight.image = imgRight[0]
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.up:
+                imgViewUp.image = imgUp[2]
+            case UISwipeGestureRecognizer.Direction.down:
+                imgViewDown.image = imgDown[2]
+            case UISwipeGestureRecognizer.Direction.left:
+                imgViewLeft.image = imgLeft[2]
+            case UISwipeGestureRecognizer.Direction.right:
+                imgViewRight.image = imgRight[2]
+            default:
+                break
+            }
+        }
+    }
+
+}
+

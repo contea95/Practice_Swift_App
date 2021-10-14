d0716ad8459b058468193151bbf3b0377185f7f7
Sang Hyeok Han <contea95@naver.com>
2021-10-14 17:35:50 +0900

    04_Scene-CustomSegueway

---
+++ Swift_Basic/04_Scene-CustomSegue/04_Scene-CustomSegue/NewSegue.swift
@@ -0,0 +1,8 @@
+//
+//  NewSegue.swift
+//  04_Scene-CustomSegue
+//
+//  Created by 한상혁 on 2021/10/14.
+//
+
+import Foundation
---
+++ Swift_Basic/04_Scene-CustomSegue/04_Scene-CustomSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  04_Scene-CustomSegue
+//
+//  Created by 한상혁 on 2021/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

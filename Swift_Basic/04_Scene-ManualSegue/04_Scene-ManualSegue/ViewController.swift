86fda5719919d196b14c25891a3c404a4ad9f95e
Sang Hyeok Han <contea95@naver.com>
2021-10-14 15:01:04 +0900

    04_Scene-ManualSegue

---
+++ Swift_Basic/04_Scene-ManualSegue/04_Scene-ManualSegue/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  04_Scene-ManualSegue
+//
+//  Created by 한상혁 on 2021/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func wind(_ sender: Any) {
+        self.performSegue(withIdentifier: "ManualWind", sender: self)
+    }
+}
+

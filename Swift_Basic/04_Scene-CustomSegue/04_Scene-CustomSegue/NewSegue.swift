d0a9e79add0aaa4e901ba4b75845663a793767e0
Sang Hyeok Han <contea95@naver.com>
2021-10-14 17:36:05 +0900

    04_Scene-CustomSegueway

--- Swift_Basic/04_Scene-CustomSegue/04_Scene-CustomSegue/NewSegue.swift
+++ Swift_Basic/04_Scene-CustomSegue/04_Scene-CustomSegue/NewSegue.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+class NewSegue: UIStoryboardSegue {
+    
+    override func perform() {
+        // 세그웨이 출발 뷰 컨트롤러
+        let srcUVC = self.source
+        // 세그웨이의 목적지 뷰 컨트롤러
+        let destUVC = self.destination
+        // fromView에서 toView로 뷰 전환
+        UIView.transition(from: srcUVC.view, to: destUVC.view, duration: 2, options: .transitionCurlUp)
+    }
+}
--- Swift_Basic/04_Scene-CustomSegue/04_Scene-CustomSegue/ViewController.swift
+++ Swift_Basic/04_Scene-CustomSegue/04_Scene-CustomSegue/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "custom_segue") {
+            NSLog("커스텀 세그 실행")
+        } else if (segue.identifier == "action_segue") {
+            NSLog("액션 세그 실행")
+        } else {
+            NSLog("알 수 없는 세그입니다.")
+        }
+//        NSLog("segueway identifier: \(segue.identifier!)")
+    }
 
 
 }

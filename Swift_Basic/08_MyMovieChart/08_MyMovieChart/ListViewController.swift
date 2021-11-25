37baaaabe97797e1e7ce7bc3bf362eb9eb646b0f
Sang Hyeok Han <contea95@naver.com>
2021-11-25 14:30:04 +0900

    09_Movie API with JSONDictionary Data

--- Swift_Basic/08_MyMovieChart/08_MyMovieChart/ListViewController.swift
+++ Swift_Basic/08_MyMovieChart/08_MyMovieChart/ListViewController.swift
@@ -10,26 +10,40 @@ import UIKit
 
 class ListViewController: UITableViewController {
     // 튜플로 구성한 데이트 세트
-    var dataset = [
-        ("다크나이트", "1 디스크립션", "2008-09-04", 8.95, "darknight.jpg"),
-        ("호우시절", "2 디스크립션", "2015-04-08", 7.91, "rain.jpg"),
-        ("말할 수 없는 비밀", "3 디스크립션", "2009-06-12", 9.99, "secret.jpg")]
+//    var dataset = [
+//        ("다크나이트", "1 디스크립션", "2008-09-04", 8.95, "darknight.jpg"),
+//        ("호우시절", "2 디스크립션", "2015-04-08", 7.91, "rain.jpg"),
+//        ("말할 수 없는 비밀", "3 디스크립션", "2009-06-12", 9.99, "secret.jpg")]
+    // 현재까지 읽어온 데이터의 페이지 정보
+    var page = 1
     
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
         
-        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            mvo.thumbnail = thumbnail
-            
-            datalist.append(mvo)
-        }
+//        for (title, desc, opendate, rating, thumbnail) in self.dataset {
+//            let mvo = MovieVO()
+//            mvo.title = title
+//            mvo.description = desc
+//            mvo.opendate = opendate
+//            mvo.rating = rating
+//            mvo.thumbnail = thumbnail
+//
+//            datalist.append(mvo)
+//        }
         return datalist
     }()
+    
+    @IBOutlet var moreBtn: UIButton!
+    
+    @IBAction func more(_ sender: Any) {
+        // 현재 페이지에 1 추가
+        self.page += 1
+        // 영화차트 API 호출
+        self.callMovieAPI()
+        // 데이터를 다시 불러오도록 데이블 뷰 갱신
+        self.tableView.reloadData()
+    }
+    
     
     override func viewDidLoad() {
         // 처음 행
@@ -59,6 +73,7 @@ class ListViewController: UITableViewController {
 //        mvo.ration = 9.19
 //        // 배열에 추가하기
 //        self.list.append(mvo)
+        self.callMovieAPI()
     }
     
     // 테이블 뷰의 개수를 반환하는 메서드
@@ -90,7 +105,14 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
-        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        
+        // 추가된 부분 : 이미지뷰 처리
+        let url: URL! = URL(string: row.thumbnail!)
+        
+        // 이미지를 읽어와 Data 객체에 저장
+        let imageData = try! Data(contentsOf: url)
+        
+        cell.thumbnail.image = UIImage(data: imageData)
         
         return cell
     }
@@ -98,4 +120,56 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
     }
+    
+    func callMovieAPI() {
+        // 1. URI 생성
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let apiURI: URL! = URL(string: url)
+        
+        // 2. REST API 호출
+        let apidata = try! Data(contentsOf: apiURI)
+        
+        // 3. 데이터 전송 결과 로그 출력
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "데이터가 없습니다."
+        NSLog("API RESULT = \(log)")
+        
+        // 4. JSON 객체 파싱해서 NSDictionary객체로 저장
+        do {
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            
+            // 5. 데이터 구조에 따라서 차례대로 파싱
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            
+            // Iterator 처리를 하면서 API DATA를 MovieVO 객체에 저장
+            for row in movie {
+                let r = row as! NSDictionary
+                
+                // 테이블 뷰 리스트 구현할 데이터 형식
+                let mvo = MovieVO()
+                
+                // movie 배열의 각 데이터를 mvo 상수의 속성에 대입
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                // list 배열 추가
+                self.list.append(mvo)
+                
+                // 7. 전체 데이터 카운트 얻기
+                let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+                
+                // 8. totalCount가 읽어온 데이터 크기와 같거나 클 경우 버튼 막기
+                if (self.list.count >= totalCount) {
+                    self.moreBtn.isHidden = true
+                }
+            }
+        } catch {
+            NSLog("Parse Error!")
+        }
+    }
+    
 }
